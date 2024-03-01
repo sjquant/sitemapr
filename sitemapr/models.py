e52e49629d6a2a7cc8965578291cf2cf234d3f01
Seonu Jang <seonujang92@gmail.com>
2024-03-01 20:47:11 +0900

    ✨ SiteMapr basic implementation (#2)
    * ➕ Install pytest
    
    * ✨ SiteMapr basic implementation

---
+++ sitemapr/models.py
@@ -0,0 +1,26 @@
+from typing import Literal
+
+from pydantic import BaseModel
+
+ChangeFreq = Literal[
+    "always", "hourly", "daily", "weekly", "monthly", "yearly", "never"
+]
+
+
+class Param(BaseModel):
+    name: str
+    values: list[str] = []
+
+
+class Page(BaseModel):
+    path: str
+    query_params: list[Param] = []
+    path_params: list[Param] = []
+
+
+class SiteMapUrl(BaseModel):
+    # Refer to https://developers.google.com/search/docs/crawling-indexing/sitemaps/build-sitemap?hl=ko#xml
+    loc: str
+    lastmod: str | None = None
+    changefreq: ChangeFreq | None = None  # Google ignores this
+    priority: float | None = None  # Google ignores this
